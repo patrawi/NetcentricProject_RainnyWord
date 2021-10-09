b4e1ba7ae709db4ca8afe8f331b75cf7089e9941
Peerawit Chariyawongsiri <>
2021-10-09 22:11:22 +0700

    generate server project

---
+++ server/typescript-node/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response} from 'express';
+
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req: Request, res: Response) => res.send('Express + TypeScript Server'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
