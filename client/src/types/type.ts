12649451e00fa68a5479cf1258970bdfa919d54f
Patrawi27 <peaphee123456789@gmail.com>
2021-10-26 21:16:30 +0700

    updateType

---
+++ client/src/types/type.ts
@@ -0,0 +1,11 @@
+export type wordToRender = {
+    id: number;
+    word: string;
+    location: string;
+    destroyed: boolean;
+  };
+
+  export type form = {
+    username: string;
+    password: string;
+}
