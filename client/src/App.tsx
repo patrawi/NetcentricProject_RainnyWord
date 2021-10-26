ae0d99014e659244e56ce5a0381dde8bb32e6003
chuwong35122 <chuwong35122@gmail.com>
2021-10-26 23:50:37 +0700

    fix bug!!!!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Home from "./views/Home";
 import Lobby from "./views/Lobby";
 import End from "./views/End";
@@ -10,68 +10,64 @@ import Footerpage from "./components/Footer";
 import Gamepage from "./views/Game";
 import AppContextProvider from "./context/AppContext";
 import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
-import SocketContextProvider from './context/SocketContext'
-
+import SocketContextProvider from "./context/SocketContext";
+import Socket from "./services/Socket";
 
 function App() {
   const [toggleDark, setToggleDark] = useState(false);
 
   const themeLight = createTheme({
-    palette : {
+    palette: {
       background: {
         default: "#DDDAE1",
-        
-
       },
-      secondary : {
-        main : "#465FB6"
-      }
-
-    }
-  })
+      secondary: {
+        main: "#465FB6",
+      },
+    },
+  });
   const themeDark = createTheme({
-    palette : {
-      background : {
-        default: "#242B2C"
+    palette: {
+      background: {
+        default: "#242B2C",
       },
-      text : {
-        primary : "#ffffff"
+      text: {
+        primary: "#ffffff",
       },
-      secondary : {
-        main : "#465FB6"
+      secondary: {
+        main: "#465FB6",
       },
-      primary : {
-        main : "#E02B31"
-      }
-
-    }
-  })
+      primary: {
+        main: "#E02B31",
+      },
+    },
+  });
   const handleToggleDark = () => {
     setToggleDark(!toggleDark);
-  }
+  };
   return (
     <>
-    <ThemeProvider theme = {toggleDark ?  themeDark : themeLight}>
-    <CssBaseline />
-    <SocketContextProvider>
-        <BrowserRouter>
-      <AppContextProvider>
-            <Navbarpage  handleToggleDark = {handleToggleDark}/>
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/lobby" component={Lobby} />
-              <Route path="/Game" component={Gamepage} />
-              <Route path="/end" component={End} />
-              <Route path="/eliminated" component={Eliminated} />
-              <Route path="/admin" component={Admin} />
-            </Switch>
-            <Footerpage />
-          </AppContextProvider>
-        </BrowserRouter>
-    </SocketContextProvider>
-
-    </ThemeProvider>
-     
+      <ThemeProvider theme={toggleDark ? themeDark : themeLight}>
+        <CssBaseline />
+        <SocketContextProvider>
+          <BrowserRouter>
+            <AppContextProvider>
+              <Navbarpage handleToggleDark={handleToggleDark} />
+              <Socket>
+                <Switch>
+                  <Route path="/" component={Home} exact />
+                  <Route path="/lobby" component={Lobby} />
+                  <Route path="/Game" component={Gamepage} />
+                  <Route path="/end" component={End} />
+                  <Route path="/eliminated" component={Eliminated} />
+                  <Route path="/admin" component={Admin} />
+                </Switch>
+              </Socket>
+              <Footerpage />
+            </AppContextProvider>
+          </BrowserRouter>
+        </SocketContextProvider>
+      </ThemeProvider>
     </>
   );
 }
